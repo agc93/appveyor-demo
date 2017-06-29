278086eb796e3565e747a1028022e972a0cc7f8e
Alistair Chapman <alistair@agchapman.com>
2017-06-29 16:45:52 +1000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace appveyor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
